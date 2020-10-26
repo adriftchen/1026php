6b56457ed878a6957d6c3d9d045ed0e7f418dd2d
linkzepp <adriftchen@gmail.com>
2020-10-26 13:15:20 +0800

    程式產生二維陣列

--- array_loop.php
+++ array_loop.php
@@ -33,3 +33,20 @@ print_r($ninenine);
 echo "<br>";
 echo $ninenine[76];
 ?>
+
+<h1>程式產生二維陣列</h1>
+
+<?php
+
+$ninenine=[];
+
+for($i=1;$i<=9;$i++){
+    for($j=1;$j<=9;$j++){
+        $ninenine[$i.$j]=$i . ' x ' . $j .' = ' .($i*$j);
+    }
+}
+print_r($ninenine);
+
+echo "<br>";
+echo $ninenine [8][9];
+?>
