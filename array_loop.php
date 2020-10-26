755195c21d80cd77017463522aa0494f735bd9e6
linkzepp <adriftchen@gmail.com>
2020-10-27 03:07:00 +0800

    樂透

--- array_loop.php
+++ array_loop.php
@@ -48,5 +48,5 @@ for($i=1;$i<=9;$i++){
 print_r($ninenine);
 
 echo "<br>";
-echo $ninenine [8][9];
+echo $ninenine ["89"];
 ?>
---
+++ grade.php
@@ -0,0 +1,5 @@
+<?php
+$score_array=array(
+  $student1=>array("chinese"=>10,"english"=>20,
+  "chemistry"=>30,"math"=>40
+),
