4d20fcfee0084472dfcee368f9630157c858b700
linkzepp <adriftchen@gmail.com>
2020-10-26 13:10:12 +0800

    以迴圈產生陣列

---
+++ array_loop.php
@@ -0,0 +1,35 @@
+<h1>以迴圈產生陣列</h1>
+<hr>
+<?php
+
+$ninenine=[];
+
+for($i=1;$i<=9;$i++){
+    for($j=1;$j<=9;$j++){
+        $ninenine[]=$i . ' x ' . $j .' = ' .($i*$j);
+    }
+}
+
+print_r($ninenine);
+
+?>
+<hr>
+<?php
+echo $ninenine[71];
+?>
+<hr>
+
+<?php
+
+$ninenine=[];
+
+for($i=1;$i<=9;$i++){
+    for($j=1;$j<=9;$j++){
+        $ninenine[$i.$j]=$i . ' x ' . $j .' = ' .($i*$j);
+    }
+}
+print_r($ninenine);
+
+echo "<br>";
+echo $ninenine[76];
+?>
